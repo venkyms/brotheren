24921a8f9b102846531a7140c6a624bf7fd1cdcc
Venkatasubramanian Subbiah <venkyms@users.noreply.github.com>
2017-09-06 00:07:37 +0530

    Intial module created for event service

---
+++ brotheren-eventservice/src/main/java/com/brotheren/brothereneventservice/BrotherenEventserviceApplication.java
@@ -0,0 +1,12 @@
+package com.brotheren.brothereneventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrotherenEventserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrotherenEventserviceApplication.class, args);
+	}
+}
